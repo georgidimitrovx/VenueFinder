e823af5af5781bc9943b61ea72296cc05440ee78
Georgi Dimitrov <gotiobg@gmail.com>
2024-02-13 16:18:54 +0200

    DDD - Renamed Server project to API - Added Application project - Added Infrastructure project - Added Tests project - Added Domain project - Implemented value types and models in Domain

---
+++ VenueFinder.Domain/Coordinates.cs
@@ -0,0 +1,25 @@
+﻿namespace VenueFinder.Domain
+{
+    /// <summary>
+    /// Coordinates Value Object
+    /// Stores the geographical location of the venue.
+    /// </summary>
+    public class Coordinates
+    {
+        public double Latitude { get; }
+        public double Longitude { get; }
+
+        public Coordinates()
+        {
+        }
+
+        public Coordinates(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+
+        // Override Equals, GetHashCode, and ToString methods
+    }
+}
